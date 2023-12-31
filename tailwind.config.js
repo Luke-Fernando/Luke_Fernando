645960b7761301c13f8b3e7dc913f28bd36c2999
Luke-Fernando <isanjaya139@gmail.com>
2024-01-01 02:00:40 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/js/*.js"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#FFFCE8",
+      secondary: "#212738",
+      // ...
+    },
+  },
+  plugins: [],
+};
